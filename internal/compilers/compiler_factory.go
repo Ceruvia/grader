af9e306eb66cce5bac7484fdd82d40c24631c19c
fawwazabrials <fawwazabrial.fas@gmail.com>
2025-05-14 11:43:56 +0700

    feat(compiler): create compiler factory

---
+++ internal/compilers/compiler_factory.go
@@ -0,0 +1,21 @@
+package compilers
+
+import (
+	"github.com/Ceruvia/grader/internal/languages"
+	"github.com/Ceruvia/grader/internal/sandboxes"
+)
+
+func CreateCompilerBasedOnLang(sandbox sandboxes.Sandbox, language languages.Language) (Compiler, error) {
+	languageName := language.GetName()
+	var (
+		comp Compiler
+		err  error
+	)
+
+	switch languageName {
+	case "c":
+		comp, err = PrepareSingleSourceFileCompiler(sandbox, language)
+	}
+
+	return comp, err
+}
