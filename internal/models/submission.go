98098ec752ec562bce66703c9168b8198042bffb
fawwazabrials <fawwazabrial.fas@gmail.com>
2025-05-20 20:57:53 +0700

    refactor: turn Submission into interface and separate them into builder and source

--- internal/models/submission.go
+++ internal/models/submission.go
@@ -1,27 +1,77 @@
 package models
 
-type Submission struct {
-	// Meta descriptions
-	Id       string
-	TempDir  string
-	Language string
-	Limits   GradingLimit
+import (
+	"github.com/Ceruvia/grader/internal/languages"
+)
 
-	// Builder related stuff (example: Makefile)
-	UseBuilder    bool
+type Submission interface {
+	IsBuilder() bool
+
+	GetId() string
+	GetEngine() string
+	GetLanguage() languages.Language
+	GetExecLanguage() languages.Language
+	GetLimits() GradingLimit
+	GetTestcases() []Testcase
+
+	GetExecFilenameOrScript() string
+	GetCompileFilenameOrScript() string
+}
+
+type SubmissionWithBuilder struct {
+	Core
 	Builder       string
 	CompileScript string
 	RunScript     string
-
-	// Non-builder related stuff
-	MainSourceFilename string
-
-	// Testcases
-	TCInputFiles  []string
-	TCOutputFiles []string
 }
 
+func (s SubmissionWithBuilder) IsBuilder() bool   { return true }
+func (s SubmissionWithBuilder) GetId() string     { return s.Core.Id }
+func (s SubmissionWithBuilder) GetEngine() string { return s.Core.Engine }
+func (s SubmissionWithBuilder) GetLanguage() languages.Language {
+	return languages.GetLanguage(s.Core.Language)
+}
+func (s SubmissionWithBuilder) GetExecLanguage() languages.Language {
+	return languages.GetLanguage(s.Builder)
+}
+func (s SubmissionWithBuilder) GetLimits() GradingLimit            { return s.Core.Limits }
+func (s SubmissionWithBuilder) GetTestcases() []Testcase           { return s.Core.Testcases }
+func (s SubmissionWithBuilder) GetExecFilenameOrScript() string    { return s.RunScript }
+func (s SubmissionWithBuilder) GetCompileFilenameOrScript() string { return s.CompileScript }
+
+type SubmissionWithFiles struct {
+	Core
+	MainSourceFilename string
+}
+
+func (s SubmissionWithFiles) IsBuilder() bool   { return false }
+func (s SubmissionWithFiles) GetId() string     { return s.Core.Id }
+func (s SubmissionWithFiles) GetEngine() string { return s.Core.Engine }
+func (s SubmissionWithFiles) GetLanguage() languages.Language {
+	return languages.GetLanguage(s.Core.Language)
+}
+func (s SubmissionWithFiles) GetExecLanguage() languages.Language {
+	return languages.GetLanguage(s.Core.Language)
+}
+func (s SubmissionWithFiles) GetLimits() GradingLimit  { return s.Core.Limits }
+func (s SubmissionWithFiles) GetTestcases() []Testcase { return s.Core.Testcases }
+func (s SubmissionWithFiles) GetExecFilenameOrScript() string {
+	return s.GetLanguage().GetExecutableFilename(s.MainSourceFilename)
+}
+func (s SubmissionWithFiles) GetCompileFilenameOrScript() string { return s.MainSourceFilename }
+
+type Core struct {
+	Id        string
+	Engine    string
+	Language  string
+	Limits    GradingLimit
+	Testcases []Testcase
+}
 type GradingLimit struct {
 	TimeInMiliseconds int
 	MemoryInKilobytes int
+}
+type Testcase struct {
+	InputFilename  string
+	OutputFilename string
 }
